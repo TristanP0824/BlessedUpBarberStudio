14271ab67e7d3244dbdbb3b5f6499acb20c27fe6
TristanP0824 <tristan.p.hernandez01@gmail.com>
2024-01-22 23:51:18 -0600

    Initial Mockup Design

---
+++ javascript/index.js
@@ -0,0 +1,3 @@
+document.getElementById('container').addEventListener('click', function() {
+    window.location.href = '../pages/home.html'; // Replace 'homepage.html' with the actual homepage URL
+});
